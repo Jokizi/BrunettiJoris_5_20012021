3b78521d4a6247c6a7e6e198538baef5b47cac7e
BRUNETTI <joris.brunetti@gmail.com>
2021-02-01 20:55:49 +0100

    add header

---
+++ frontend/js/catalogue.js
@@ -0,0 +1,9 @@
+const myFunction = () => {
+    const produits = request('http://localhost:3000/api/teddies')
+        produits.then((teddies) => {
+            teddies.forEach((teddie) => {
+                afficheProduit(teddie)
+            })
+    })
+}
+myFunction();
